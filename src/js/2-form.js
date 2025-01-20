830b53d88cd1a36026f1a2ea6040054369f879d6
ChelDI <bomgmasluk@gmail.com>
2025-01-20 08:23:27 +0200

    rework

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,61 +1,39 @@
 const formState = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
 
-const localState = JSON.parse(localStorage.getItem(formState));
+let formData = { email: "", message: "" };
 
+const localState = JSON.parse(localStorage.getItem(formState));
 if (localState) {
-  for (const key of Object.keys(localState)) {
-    document.querySelector(`[name="${key}"]`).value = localState[key];
-  }
+  formData = localState;
+  document.querySelector('[name="email"]').value = formData.email;
+  document.querySelector('[name="message"]').value = formData.message;
 }
 
 form.addEventListener('input', onInputSaveToLocalStorage);
 
 form.addEventListener('submit', onSubmitForm);
 
-function onInputSaveToLocalStorage(event) {
-  const key = event.target.name;
-  const updatedStorage = {
-    ...JSON.parse(localStorage.getItem(formState)),
-    [key]: event.target.value.trim(),
-  };
 
-  localStorage.setItem(formState, JSON.stringify(updatedStorage));
+function onInputSaveToLocalStorage(event) {
+  const { name, value } = event.target;
+  formData[name] = value.trim(); 
+
+  localStorage.setItem(formState, JSON.stringify(formData));
 }
 
 function onSubmitForm(event) {
   event.preventDefault();
 
-  const formData = new FormData(event.target);
-  const formDataObj = Object.fromEntries(formData.entries());
-  if (validateFormFields(formDataObj)) {
-    console.log('submit', formDataObj);
-
-    localStorage.removeItem(formState);
-
-    event.target.reset();
-  }
-}
-
-function validateFormFields(formDataObj) {
-  let isValid = true;
-  for (const key in formDataObj) {
-    if (!formDataObj[key]) {
-      addBorderInputError(document.querySelector(`[name="${key}"]`));
-      isValid = false;
-    }
-    if (formDataObj[key]) {
-      removeBorderInputError(document.querySelector(`[name="${key}"]`));
-    }
+  if (formData.email === "" || formData.message === "") {
+    alert("Fill please all fields");
+    return;
   }
 
-  return isValid;
-}
+  console.log('Form submitted with data:', formData);
 
-function addBorderInputError(input) {
-  input.classList.add('error');
-}
+  localStorage.removeItem(formState);
+  formData = { email: "", message: "" };
 
-function removeBorderInputError(input) {
-  input.classList.remove('error');
+  form.reset();
 }
